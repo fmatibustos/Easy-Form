ada1d8a3ef4c87c87c8f5cc93755d8197f844146
Mati Bustos <matiasbustos90@gmail.com>
2021-08-31 20:32:08 -0300

    editing js file

--- form.js
+++ form.js
@@ -1,7 +1,8 @@
 const form = document.getElementById("user-form");
 /* console.log(form); */
-
 const submitButton = document.getElementById("submit-btn");
+
+let timeout= null; 
 
 // Escribo un bucle para iterar, para poder ir agregando eventlisteners.
 
@@ -10,7 +11,34 @@ document.querySelectorAll(".form-box").forEach((box)=>{
     const boxInput = box.querySelector("input");
 
 
-    boxInput.addEventListener("keydown", (e)=>{
-        console.log(`input ${boxInput.name} value:`, boxInput.value);
+boxInput.addEventListener("keydown", (e)=>{
+        clearTimeout(timeout);
+        timeout= setTimeout(() => {
+            console.log(`input ${boxInput.name} value:`, boxInput.value);
+
+            validation(box, boxInput)
+
+        }, 300); 
     });
 });
+
+validation = ( box, boxInput) => {
+    if (boxInput.name=='nickName') {
+        console.log('validacion para nombre');
+        if (boxInput.value == '') {
+            showError (true, box);
+        } else  {
+            showError (false, box);
+        }
+    }
+}
+
+showError = (check, box) => {
+    if (check) {
+        box.classList.remove('form-success');
+        box.classList.add('form-error');
+    } else {
+        box.classList.remove ('form-error');
+        box.classList.add('form-success');
+    }
+};
