29e617cd18ff716ada2a1ecc72d53ae4734968ba
Mati Bustos <matiasbustos90@gmail.com>
2021-09-11 16:04:04 -0300

    css

--- form.js
+++ form.js
@@ -3,7 +3,9 @@ const form = document.getElementById("user-form");
 const submitButton = document.getElementById("submit-btn");
 
 let timeout= null; 
-
+let errors = {
+    nickName: true,
+}
 // Escribo un bucle para iterar, para poder ir agregando eventlisteners.
 
 // Seleccionamos con un querySelectorAll cada form box que existen en el form y por cada uno de ellos  tendremos un box y haremos lo siguiente en la funcion: 
@@ -11,7 +13,7 @@ document.querySelectorAll('.form-box').forEach((box)=>{
     const boxInput = box.querySelector("input");
 
 
-boxInput.addEventListener("keydown", (e)=>{
+boxInput.addEventListener("keydown", (event)=>{
         clearTimeout(timeout);
         timeout= setTimeout(() => {
             console.log(`input ${boxInput.name} value:`, boxInput.value);
@@ -24,21 +26,33 @@ boxInput.addEventListener("keydown", (e)=>{
 
 validation = ( box, boxInput) => {
     if (boxInput.name == 'nickName') {
-        console.log('validacion para nombre');
         if (boxInput.value == '') {
-            showError (true, box);
+            showError (true, box, boxInput);
         } else  {
-            showError (false, box);
+            showError (false, box, boxInput);
         }
     }
-}
+    submitController();
+};
 
-showError = (check, box) => {
+showError = (check, box, boxInput) => {
     if (check) {
         box.classList.remove('form-success');
         box.classList.add('form-error');
+        errors [boxInput.name] = true;
     } else {
         box.classList.remove ('form-error');
         box.classList.add('form-success');
+        errors [boxInput.name] = false;
     }
 };
+
+submitController = () => {
+    console.log(errors);
+    if (errors.nickName){
+        submitButton.toggleAttribute("disabled", true)
+    } else 
+    {
+        submitButton.toggleAttribute("disabled", false)
+    }
+}
