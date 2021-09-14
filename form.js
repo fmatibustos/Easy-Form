a6ea8a298963c648a07c64879d48d82f99ff6946
Mati Bustos <matiasbustos90@gmail.com>
2021-09-14 10:58:06 -0300

    last edit

--- form.js
+++ form.js
@@ -1,58 +1,81 @@
-const form = document.getElementById("user-form");
-/* console.log(form); */
-const submitButton = document.getElementById("submit-btn");
+const form = document.getElementById('user-form');
+const submitButton = document.getElementById('submit-btn');
 
-let timeout= null; 
+let timeout = null;
+
 let errors = {
-    nickName: true,
-}
-// Escribo un bucle para iterar, para poder ir agregando eventlisteners.
+  nickName: true,
+  email: true,
+  password: true,
+};
 
-// Seleccionamos con un querySelectorAll cada form box que existen en el form y por cada uno de ellos  tendremos un box y haremos lo siguiente en la funcion: 
-document.querySelectorAll('.form-box').forEach((box)=>{
-    const boxInput = box.querySelector("input");
+const mailformatRegex = /^[^@]+@\w+(\.\w+)+\w$/;
 
+document.querySelectorAll('.form-box').forEach((box) => {
+  const boxInput = box.querySelector('input');
 
-boxInput.addEventListener("keydown", (event)=>{
-        clearTimeout(timeout);
-        timeout= setTimeout(() => {
-            console.log(`input ${boxInput.name} value:`, boxInput.value);
+  boxInput.addEventListener('keydown', (event) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      console.log(`Input ${boxInput.name} value: `, boxInput.value);
 
-            validation(box, boxInput)
-
-        }, 300); 
-    });
+      validation(box, boxInput);
+    }, 300);
+  });
 });
 
-validation = ( box, boxInput) => {
-    if (boxInput.name == 'nickName') {
-        if (boxInput.value == '') {
-            showError (true, box, boxInput);
-        } else  {
-            showError (false, box, boxInput);
-        }
+validation = (box, boxInput) => {
+  if (boxInput.value == '') {
+    showError(true, box, boxInput);
+  } else {
+    showError(false, box, boxInput);
+  }
+
+  if (boxInput.name == 'email') {
+    if (!boxInput.value.match(mailformatRegex)) {
+      showError(true, box, boxInput);
+    } else {
+      showError(false, box, boxInput);
     }
-    submitController();
+  }
+
+  if (boxInput.name == 'password') {
+    if (boxInput.value.length <= 6) {
+      showError(true, box, boxInput);
+    } else {
+      showError(false, box, boxInput);
+    }
+  }
+
+  submitController();
 };
 
 showError = (check, box, boxInput) => {
-    if (check) {
-        box.classList.remove('form-success');
-        box.classList.add('form-error');
-        errors [boxInput.name] = true;
-    } else {
-        box.classList.remove ('form-error');
-        box.classList.add('form-success');
-        errors [boxInput.name] = false;
-    }
+  if (check) {
+    box.classList.remove('form-success');
+    box.classList.add('form-error');
+    errors[boxInput.name] = true;
+  } else {
+    box.classList.remove('form-error');
+    box.classList.add('form-success');
+    errors[boxInput.name] = false;
+  }
 };
 
 submitController = () => {
-    console.log(errors);
-    if (errors.nickName){
-        submitButton.toggleAttribute("disabled", true)
-    } else 
-    {
-        submitButton.toggleAttribute("disabled", false)
-    }
-}
+  console.log(errors);
+  if (errors.nickName || errors.email || errors.password) {
+    submitButton.toggleAttribute('disabled', true);
+  } else {
+    submitButton.toggleAttribute('disabled', false);
+  }
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  console.log([...formData]);
+  for (let [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
+});
